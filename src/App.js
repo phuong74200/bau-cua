727adec9cdfaa0727e5bb8c02a3da4d35976c9af
Phuong Thanh <phuong74200@gmail.com>
2022-01-11 22:19:16 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Login } from './component';
+
+function App() {
+    return (
+        <div>
+            <Login />
+        </div>
+    );
+}
+
+export default App;
