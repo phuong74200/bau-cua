0ccb136be754864c432c860253af9d1e9ac92a64
LeXuanDai0307 <lxdai0307@gmail.com>
2022-01-12 10:23:12 +0700

    Create branch develop

--- src/component/Login/index.js
+++ src/component/Login/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { StyledLogin } from './index.style.js';
 
 const Login = () => {
-    return <StyledLogin>Login Page here</StyledLogin>;
+    return <StyledLogin>Login Page here!</StyledLogin>;
 };
 
 export default Login;
