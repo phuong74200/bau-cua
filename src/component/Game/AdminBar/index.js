8bf4c135e66dafe7d502772a567918c29c230729
Phuong Thanh <phuong74200@gmail.com>
2022-01-17 11:36:45 +0700

    change reset

--- src/component/Game/AdminBar/index.js
+++ src/component/Game/AdminBar/index.js
@@ -24,7 +24,7 @@ const Roll = ({ setRank }) => {
             })
             .then((res) => {
                 _axios
-                    .post(`/room/${searchParams.get('roomID')}/reset-and-get-rank`, {
+                    .post(`/room/${searchParams.get('roomID')}/reset`, {
                         id: searchParams.get('roomID'),
                     })
                     .then((ranking) => {
